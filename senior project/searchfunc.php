5883bb9f20c12a0270035ae924478866e4d89531
Clinton Harding <112673490+ClintHarding@users.noreply.github.com>
2023-08-31 18:28:46 -0400

    Delete senior project directory

--- senior project/searchfunc.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $query = mysql_query("select * from data where id_user = '$id_user' order by date asc");
-    $i = 0;
-    $options = array();
-    while($data = mysql_fetch_array($query)){
-       $options[$data['id']] =  $data['name'];
-    }
-    echo json_encode($options);
-?>
