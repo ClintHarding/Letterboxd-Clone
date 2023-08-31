9284025fcdbf4dfb68ae4d6a3da2af3a6c6531c5
Clinton Harding <112673490+ClintHarding@users.noreply.github.com>
2023-08-31 18:33:00 -0400

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -3,11 +3,11 @@
 	if(!defined("IN_CODE")) {
         die("not an entry point");
         }
-
-        $host="cinescore.clumhpei3vvp.us-east-2.rds.amazonaws.com";
-        $username="admin";
-        $password="cinema123";
-        $dbname="cinescore";
+	//connection details to AWS database
+        $host="";
+        $username="";
+        $password="";
+        $dbname=";
 
 
 
